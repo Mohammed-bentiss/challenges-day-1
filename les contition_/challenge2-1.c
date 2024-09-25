e62f6faf58661bebd3fa484eae3aef37af6ca1c5
mohammed-bentiss <arttissdecoratin@gmail.com>
2024-09-25 17:45:37 +0200

    test

---
+++ les contition_/challenge2-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int nomber;
+    printf ("entre le nomber: ");
+    scanf("%d",&nomber);
+    if (nomber%2==0)
+    printf ("nomber pair");
+    else 
+    printf ("nomber impair");
+    
+    
+
+    return 0;
+}
