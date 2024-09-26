bbfd947d007bb2254ed002f2905c386ea6272a67
mohammed-bentiss <arttissdecoratin@gmail.com>
2024-09-26 18:14:57 +0200

    challeng

---
+++ challenges day 1/les boucle/challenge5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n,p,i;
+    printf ("entre un nomber: ");
+    scanf("%d",&n);
+    printf("entre la puissance un nomber: ");
+    scanf("%d",&p);
+    i=n*p;
+    printf ("%d",i);
+        
+    
+    return 0;
+}
