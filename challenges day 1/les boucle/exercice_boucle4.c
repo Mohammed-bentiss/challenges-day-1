d47b24f27006fa704e213c868352f035301ec9be
mohammed-bentiss <arttissdecoratin@gmail.com>
2024-09-26 16:25:57 +0200

    exercice boucle 4

---
+++ challenges day 1/les boucle/exercice_boucle4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n,T,i;
+    printf (" entre un number: ");
+    scanf("%d",&n);
+    
+    for(i = 0 ; i<n ; i++){
+       
+        printf("%d\n", i*2+1);
+        
+    }
+    
+
+    return 0;
+}
