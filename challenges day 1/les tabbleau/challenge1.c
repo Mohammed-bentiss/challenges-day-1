9e3609b23b14edf766e211d36ea2a68e18ba7c64
mohammed-bentiss <arttissdecoratin@gmail.com>
2024-09-27 11:16:32 +0200

    challenga du tableau 1

---
+++ challenges day 1/les tabbleau/challenge1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int tableu_du_nomber[] ={1,2,3,4,5,6,7,8,9};
+      for (i>0;i<9;i++)
+       printf("%d\n",tableu_du_nomber[i]);
+    
+
+    return 0;
+}
