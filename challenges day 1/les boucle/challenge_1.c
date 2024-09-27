b096cfa222926b33b92640827b5c626abe9a9941
Mohammed-bentiss <arttissdecoration@gmail.com>
2024-09-27 22:16:58 +0100

    Update challenge_1.c
    Challenge 1 : Table de Multiplication
    Écrivez un programme C qui demande un nombre à l’utilisateur et affiche sa table de multiplication de 1 à 10. Par exemple, pour le nombre 4, affichez :
    
    4 * 1 = 4 4 * 2 = 8 4 * 3 = 12 ... 4 * 10 = 40

--- challenges day 1/les boucle/challenge_1.c
+++ challenges day 1/les boucle/challenge_1.c
@@ -4,8 +4,8 @@ int main() {
     int a,b;
     printf("entre un nomber: ");
     scanf("%d",&b);
-    for(a=0;a<=10;a++)
-    printf("%d * %d = %d\n",a,b,a*b);
-   
+    for(a=0;a<=10;a++){
+        printf("%d x %d = %d\n",a,b,a*b);
+    }
     return 0;
 }
